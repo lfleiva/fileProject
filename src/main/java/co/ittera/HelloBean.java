952dafed40cbc7e72d0ac3a577d95aa2a4acf63f
lfleiva <luisfernandoleivab@gmail.com>
2017-07-10 04:42:17 -0500

    Implementacion inicial con Google App Engine

---
+++ src/main/java/co/ittera/HelloBean.java
@@ -0,0 +1,30 @@
+package co.ittera;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import java.io.Serializable;
+
+@ManagedBean
+@SessionScoped
+public class HelloBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@PostConstruct
+	public void init() {
+				
+	}
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
